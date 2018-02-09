20745af1a27de8a2b3b8955d4cd8ca4dd38d4bcd
Erin Fox <erinkayfox@gmail.com>
2018-02-09 10:02:45 -0500

    add slides and images

--- presentation/index.js
+++ presentation/index.js
@@ -1,70 +1,149 @@
 // Import React
-import React from "react";
+import React from 'react';
 
 // Import Spectacle Core tags
 import {
+  Appear,
   BlockQuote,
   Cite,
+  CodePane,
+  Code,
   Deck,
+  Fill,
+  Fit,
   Heading,
+  Image,
+  Layout,
   ListItem,
   List,
   Quote,
   Slide,
-  Text
-} from "spectacle";
+  Text,
+} from 'spectacle';
 
 // Import theme
-import createTheme from "spectacle/lib/themes/default";
+import createTheme from 'spectacle/lib/themes/default';
 
 // Require CSS
-require("normalize.css");
+require('normalize.css');
 
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quarternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+  {
+    primary: 'white',
+    secondary: '#1F2022',
+    tertiary: '#03A9FC',
+    quarternary: '#CECECE',
+  },
+  {
+    primary: 'Montserrat',
+    secondary: 'Helvetica',
+  }
+);
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+      <Deck
+        transition={['zoom', 'slide']}
+        transitionDuration={500}
+        theme={theme}
+      >
+        {/* intro slide */}
+        <Slide transition={['zoom']} bgColor="primary">
+          <Heading size={2} fit caps lineHeight={1} textColor="secondary">
+            Array Methods
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+            erin fox
+          </Text>
+          <Text margin="10px 0 0" textColor="tertiary" size={1}>
+            @erinfoox
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        {/* What i'll be explaining */}
+        <Slide bgColor="primary">
+          <Heading size={1} textColor="secondary">
+            What is an Array Method?
+          </Heading>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
+        <Slide bgColor="primary">
+          <Heading size={1} textColor="secondary">
+            Why use an Array Method?
+          </Heading>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+        {/* .map method */}
+        <Slide bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            .map
+          </Heading>
+        </Slide>
+        <Slide bgColor="primary">
+          <Image
+            height={200}
+            width={425}
+            src={require('../assets/array-example.jpg')}
+          />
+          <Image src={require('../assets/map-examples.jpg')} />
+        </Slide>
+        {/* .filter method */}
+        <Slide bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            .filter
+          </Heading>
+        </Slide>
+        <Slide>
+          <Image
+            height={200}
+            width={425}
+            src={require('../assets/array-example.jpg')}
+          />
+          <Image
+            height={450}
+            width={550}
+            src={require('../assets/filter-examples.jpg')}
+          />
+        </Slide>
+        {/* .find method */}
+        <Slide bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            .find
+          </Heading>
+        </Slide>
+        <Slide>
+          <Image
+            height={200}
+            width={425}
+            src={require('../assets/array-example.jpg')}
+          />
+          <Image
+            height={450}
+            width={720}
+            src={require('../assets/find-examples.jpg')}
+          />
+        </Slide>
+        {/* .reduce method */}
+        <Slide bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            .reduce
+          </Heading>
+        </Slide>
+        <Slide>
+          <Image
+            height={300}
+            width={200}
+            src={require('../assets/reduce-array-example.jpg')}
+          />
+          <Image
+            height={220}
+            width={700}
+            src={require('../assets/reduce-examples.jpg')}
+          />
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={3} textColor="secondary">
+            THE END.
+          </Heading>
         </Slide>
       </Deck>
     );
